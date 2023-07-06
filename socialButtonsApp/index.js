29e07e370186180b0b78bba8f0ec9cd84d58c18b
Lavanya-Vemulakonda <133259982+Lavanya-Vemulakonda@users.noreply.github.com>
2023-07-06 11:04:50 +0530

    Update index.js

--- socialButtonsApp/index.js
+++ socialButtonsApp/index.js
@@ -1,11 +1,11 @@
 const Button = (props) => {
-  //  Write your code here.
+  
   const { className, buttonText } = props;
-  return <button className={`button ${className}`}>{buttonText}</button>;
+  return <button className={`button {className}`}>{buttonText}</button>;
 };
 
 const element = (
-  //  Write your code here.
+  
   <div className="social-buttons-container">
     <div className="content-container">
       <h1 className="heading">Social Buttons</h1>
